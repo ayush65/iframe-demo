3fd4c482486007dc4d1465117d435f39f712bf7c
Ayush65 <payush381@gmail.com>
2023-08-22 15:18:59 +0530

    seconfd commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <div>
+        {" "}
+        <iframe
+          src="https://storage.net-fs.com/hosting/2727323/374/index.htm"
+          title="programiz pro website"
+          height="600"
+          width="1500"
+        ></iframe>
+      </div>
       <iframe
         src="https://carbon.now.sh/embed?bg=rgba%28208%2C2%2C27%2C1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
         styles="width: 100px; height: 1000px;"
@@ -11,13 +20,6 @@ function App() {
 
       <iframe
         src="https://programiz.pro"
-        title="programiz pro website"
-        height="500"
-        width="500"
-      ></iframe>
-
-      <iframe
-        src="https://storage.net-fs.com/hosting/2727323/374/index.htm"
         title="programiz pro website"
         height="500"
         width="500"
