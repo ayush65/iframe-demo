5d7bf22e928fb8173588b56d4994e16a429a43d7
Ayush65 <payush381@gmail.com>
2023-08-22 15:13:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://carbon.now.sh/embed?bg=rgba%28208%2C2%2C27%2C1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+        styles="width: 100px; height: 1000px;"
+      ></iframe>
+
+      <iframe
+        src="https://programiz.pro"
+        title="programiz pro website"
+        height="500"
+        width="500"
+      ></iframe>
+
+      <iframe
+        src="https://storage.net-fs.com/hosting/2727323/374/index.htm"
+        title="programiz pro website"
+        height="500"
+        width="500"
+      ></iframe>
     </div>
   );
 }
