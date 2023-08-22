c49f7eee3f84f2a0fdb9183cb40b55473fa7e540
Ayush65 <payush381@gmail.com>
2023-08-22 15:21:54 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ function App() {
           width="1500"
         ></iframe>
       </div>
+      <div>
+        {" "}
+        <iframe
+          src="https://www.3dvista.com/samples/new_york_loft.html"
+          title="programiz pro website"
+          height="600"
+          width="1500"
+        ></iframe>
+      </div>
       <iframe
         src="https://carbon.now.sh/embed?bg=rgba%28208%2C2%2C27%2C1%29&t=seti&wt=none&l=auto&width=680&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false&code=helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
         styles="width: 100px; height: 1000px;"
